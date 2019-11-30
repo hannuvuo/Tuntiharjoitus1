21fce018f3c6fba3775771470e8392d40a4f643c
hannuvuo <hannu.vuorenmaa@seamk.fi>
2019-11-30 12:51:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuntiharjoitus1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Testi, git-webissä tehty muutos
+            Console.WriteLine("Hello world");
+
+            //koneella tehty muutos git-webissä tehdyn muutoksen jälkeen.			
+
+            Console.ReadLine();
+        }
+    }
+}
